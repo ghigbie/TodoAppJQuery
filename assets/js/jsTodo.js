fe8c1ca15c92354a4fddc9260b0e994a98c4e98e
George Higbie <gehigbie@gmail.com>
2017-01-24 23:36:33 -0700

    cleared comments from js file

--- assets/js/jsTodo.js
+++ assets/js/jsTodo.js
@@ -1,21 +1,3 @@
-//created in terminal
-//alert("JS Connected!"); //-used to verify that JS file is connected
-
-//Check off specifi todos by clicking
-// $("li").click(function(){
-// 	if($(this).css("color") === "rgb(128, 128, 128)"){
-// 		$(this).css({
-// 			color: "#000000",
-// 			textDecoration: "none"
-// 		});
-// 	}else{
-// 		$(this).css({
-// 			color: "gray",
-// 			textDecoration: "line-through"
-// 		});
-// 	}
-// });
-
 $("ul").on("click", "li", function(){
 	$(this).toggleClass("completed");
 });
