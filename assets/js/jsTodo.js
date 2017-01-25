d2d1b9ff812a1a035111fe2f991ec5120ea1d5ef
George Higbie <gehigbie@gmail.com>
2017-01-24 21:38:54 -0700

    added on click event to span

--- assets/js/jsTodo.js
+++ assets/js/jsTodo.js
@@ -2,16 +2,25 @@
 //alert("JS Connected!"); //-used to verify that JS file is connected
 
 //Check off specifi todos by clicking
+// $("li").click(function(){
+// 	if($(this).css("color") === "rgb(128, 128, 128)"){
+// 		$(this).css({
+// 			color: "#000000",
+// 			textDecoration: "none"
+// 		});
+// 	}else{
+// 		$(this).css({
+// 			color: "gray",
+// 			textDecoration: "line-through"
+// 		});
+// 	}
+// });
+
 $("li").click(function(){
-	if($(this).css("color") === "rgb(128, 128, 128)"){
-		$(this).css({
-			color: "#000000",
-			textDecoration: "none"
-		});
-	}else{
-		$(this).css({
-			color: "gray",
-			textDecoration: "line-through"
-		});
-	}
+	$(this).toggleClass("completed");
+});
+
+//click on X to delete todo
+$("span").click(function(){
+	alert("clicked on the span");
 });
